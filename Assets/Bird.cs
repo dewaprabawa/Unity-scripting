f8732dabea53b78b9b3cc9ce662046c986a55cca
dewa <dewaprabawa>
2020-09-19 23:02:43 +0800

    lazy progress today sorry

--- Assets/Bird.cs
+++ Assets/Bird.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class Bird : MonoBehaviour
 {
     // Start is called before the first frame update
-    
+    private Vector3 initialPosition;
+
+    private void Awake(){
+        initialPosition = transform.position;
+    }
+
     private void OnMouseDown() {
        GetComponent<SpriteRenderer>().color = Color.red;
     }
 
     private void OnMouseUp(){
         GetComponent<SpriteRenderer>().color = Color.white;
+
+        Vector2 directionToInitialDirection = initialPosition - transform.position;
+        GetComponent<Rigidbody2D>().AddForce(directionToInitialDirection);
     }
 
     private void OnMouseDrag()
