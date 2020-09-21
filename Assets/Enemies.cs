9315314d6f5b7ef96195826bdb80b711e8252af5
dewa <dewaprabawa>
2020-09-21 23:49:45 +0800

    add enemies logic

---
+++ Assets/Enemies.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Bird bird = other.collider.GetComponent<Bird>();
+        if (bird != null){
+            Destroy(gameObject);
+        }
+
+        Enemies enemies = other.collider.GetComponent<Enemies>();
+        if (enemies != null){
+            return;
+        }
+
+        if (other.contacts[0].normal.y < -0.5){
+            Destroy(gameObject);
+        }
+    }
+}
